ce15dd05890691330c5595ab47698ce854ed8b93
Chuanxun Su <suchuanxun@163.com>
2020-07-16 16:01:46 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='spap',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     # package_dir={'':'spap'},
     url='https://github.com/chuanxun/StructurePrototypeAnalysisPackage',
